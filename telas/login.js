a8de60460a6e6f9b637193a32dbbb85c1aebd69d
GabrielVianaSantos <polvohumano10@gmail.com>
2024-02-20 21:16:13 -0300

    Tela Login

--- telas/login.js
+++ telas/login.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import Topo from "./components/topo";
 import Botao from "./components/botao";
 
@@ -8,12 +8,28 @@ export default function Login(){
         <Topo {...Topo}/>
         <View>
             <Text>Login</Text>
+            <Text>Usuário</Text>
+            <Text>Email</Text>
+            <Text>Senha</Text>
         </View>
+
+        <TouchableOpacity>
+            <Text>Esqueceu a senha?</Text>
+        </TouchableOpacity>
+
+        
+        <TouchableOpacity>
+            <Text>Entre com o Google</Text>
+        </TouchableOpacity>
+
         <Botao texto= 'Entrar' {...Botao}/>
+
+        <TouchableOpacity>
+            <Text>Não tem uma senha?</Text>
+        </TouchableOpacity> 
     </>
 }
 
 const estilos = StyleSheet.create({
-
 
 })
