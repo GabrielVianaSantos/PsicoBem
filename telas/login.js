459b92b6832ad35219ce18f62e031e60f7165668
GabrielVianaSantos <polvohumano10@gmail.com>
2024-02-21 19:41:46 -0300

    Estilos Login

--- telas/login.js
+++ telas/login.js
@@ -1,35 +1,60 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, TextInput } from "react-native";
 import Topo from "./components/topo";
 import Botao from "./components/botao";
 
-export default function Login(){
+export default function Login(botao){
     return<>
         <Topo {...Topo}/>
-        <View>
+        <View style = {estilos.container}>
+            
+            <View style = {estilos.center}>
             <Text>Login</Text>
+            </View>
+            
             <Text>Usuário</Text>
+            <TextInput></TextInput>
             <Text>Email</Text>
+            <TextInput></TextInput>
             <Text>Senha</Text>
-        </View>
+            <TextInput></TextInput>
+        
 
-        <TouchableOpacity>
+        <TouchableOpacity style = {estilos.centerBotoes}>
             <Text>Esqueceu a senha?</Text>
         </TouchableOpacity>
-
         
-        <TouchableOpacity>
+        <TouchableOpacity style = {estilos.centerBotoes}>
             <Text>Entre com o Google</Text>
         </TouchableOpacity>
 
-        <Botao texto= 'Entrar' {...Botao}/>
+        <Botao texto="Entrar" {...Botao}/>
 
-        <TouchableOpacity>
+        <TouchableOpacity style = {estilos.centerBotoes}>
             <Text>Não tem uma senha?</Text>
         </TouchableOpacity> 
+        </View>
     </>
 }
 
 const estilos = StyleSheet.create({
+
+    container:{
+        padding: "10%",
+    },
+
+    center: {
+        alignItems: "center",
+        paddingTop: "20%",
+        paddingBottom: "15%",
+    },
+
+    centerBotoes: {
+        alignItems: "center",
+        paddingTop: "5%",
+        paddingBottom: "5%",
+    },
+
+
 
 })
